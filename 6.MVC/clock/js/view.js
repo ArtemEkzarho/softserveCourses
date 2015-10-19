71cf614d6de0d673154cf71b7871791cc7cc5641
Artem <sru_529@mail.ru>
2015-10-19 12:15:17 +0300

    fixed indents
    fixed indents

--- 6.MVC/clock/js/view.js
+++ 6.MVC/clock/js/view.js
@@ -1,14 +1,14 @@
 'use strict';
 
 function View(clockWrap) {
-	this.addClock = function(state) {
-		var date = new Date,
-        hours = date.getHours(),
-        minutes = date.getMinutes(),
-        seconds = date.getSeconds(),
-        month = date.getMonth() + 1,
-    	day = date.getDate(),
-    	year = date.getFullYear();
+    this.addClock = function(state) {
+        var date = new Date,
+            hours = date.getHours(),
+            minutes = date.getMinutes(),
+            seconds = date.getSeconds(),
+            month = date.getMonth() + 1,
+    	    day = date.getDate(),
+    	    year = date.getFullYear();
 
 	   	if (state === 'time') {
 	   		clockWrap.className = 'time';
