8645b03ba2833ea3d8109d6152d5323b3d89be49
Artem <artem.ekzarho@gmail.com>
2015-10-24 23:58:07 +0300

    success! first working version

--- 7.Qunit/Project/js/main.js
+++ 7.Qunit/Project/js/main.js
@@ -3,61 +3,57 @@
 document.addEventListener("DOMContentLoaded", main, false);
 
 function main () {
-	console.log(toPalindrome(12321));
+	console.log(toPalindrome(5));
 } 
 
 function toPalindrome (number) {
-	var numbers = String(number).split(''),
-		halfLength = numbers.length / 2,
-		centerIndex = halfLength.toFixed() - 1,
-		countdown = centerIndex,
-		result = [],
-		i,
-		newNumber;
-
-	if (numbers.length === 1) {
-		result = 11;
-	} else {
-		if (numbers.length%2 === 0) {
-			for (i = 0; i < numbers.length; i++) {
-				if (i > centerIndex) {
-					if (i === centerIndex + 1) {
-						if (numbers[i + 1] > numbers[i]) {
-							result.push(+numbers[i - 1] + 1);
-						} else {
-							result.push(+numbers[i - 1]);
-						}
-						continue;
-					}
-
-					countdown--;
-					result.push(+numbers[countdown]);
-
-				} else if (i === centerIndex) {
-					if (numbers[i + 1] > numbers[i]) {
-						result.push(+numbers[i] + 1);
-					} else {
-						result.push(+numbers[i]);
-					}
-				} else {
-					result.push(+numbers[i]);
-				}
-			}
-
-			result = result.join('');
-		} else {
-			for (i = 0; i < numbers.length; i++) {
-				if (i > centerIndex) {
-					countdown--;
-					result.push(+numbers[countdown]);
-				} else {
-					result.push(+numbers[i]);
-				}
-			}
-
-			result = result.join('');
-		}
+	var result = number;
+	
+	while (isPalindrom(result) === false) {
+	  console.log(result);
+	  result++;
 	}
 
 	return result;
+}
+
+function isPalindrom (number) {
+	var numbers = String(number).split(''),
+		halfLength = numbers.length / 2,
+		centerIndex = halfLength.toFixed() - 1,
+		i,
+		countdown = centerIndex,
+		result;
+
+		if (numbers.length === 1) {
+			result = false;
+		} else {
+			if (numbers.length%2 === 0) {
+				for (i = 0; i < numbers.length; i++) {
+					if (i > centerIndex) {
+						if (numbers[i] === numbers[countdown]) {
+							result = true;
+						} else {
+							result = false;
+							break;
+						}
+						countdown--;
+					}
+				} 
+			} else {
+				for (i = 0; i < numbers.length; i++) {
+					if (i > centerIndex) {
+						countdown--;
+						if (numbers[i] === numbers[countdown]) {
+							result = true;
+						} else {
+							result = false;
+							break;
+						}
+					}
+				}
+			}			
+		}
+
+	return result;	
 }
