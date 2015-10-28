2cf4c3fcb4172742a4e763852e4e6474481756f6
Artem <artem.ekzarho@gmail.com>
2015-10-28 02:38:44 +0200

    refactoring

--- 6.MVC/list/libs/helpers.js
+++ 6.MVC/list/libs/helpers.js
@@ -1,5 +1,12 @@
 'use strict'
 
-function getEl (selector) {
-    return document.querySelector(selector);
-}
+var helpers = (function (){
+
+	function getEl (selector) {
+    	return document.querySelector(selector);
+	}
+
+	return {
+		getEl: getEl
+	}
+}());
