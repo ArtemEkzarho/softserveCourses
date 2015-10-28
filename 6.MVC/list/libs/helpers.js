da8bc69af11b579ced470db08b12a5bf6f64c94d
Artem <sru_529@mail.ru>
2015-10-28 03:28:20 +0200

    refactoring

--- 6.MVC/list/libs/helpers.js
+++ 6.MVC/list/libs/helpers.js
@@ -2,15 +2,9 @@
 
 var helpers = (function (){
 
-<<<<<<< HEAD
     function getEl (selector) {
         return document.querySelector(selector);
     }
-=======
-	function getEl (selector) {
-    	        return document.querySelector(selector);
-	}
->>>>>>> origin/master
 
     return {
         getEl: getEl
