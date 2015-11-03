2bce48cf0683c2f1d4f94aa966ddf84b0f496261
Artem <artem.ekzarho@gmail.com>
2015-11-03 11:57:47 +0200

    refactoring

--- 8.Editable list/js/tableController.js
+++ 8.Editable list/js/tableController.js
@@ -1,7 +1,50 @@
 'use strict';
 
 function TableController (group) {
-    var tableWrap = helpers.getEl('#students tbody'),
+	var students = [{
+	        id: 0,
+	        name: 'Artem',
+	        surname: 'Ekzarkho', 
+	        gender: 'boy'
+	    },{
+	        id: 1,
+	        name: 'Yevheniia', 
+	        surname: 'Kryschyk',
+	        gender: 'girl'
+	    },{
+	        id: 2,
+	        name: 'Yulyia',
+	        surname: 'Lur\'eva',
+	        gender: 'girl'
+	    },{
+	        id: 3,
+	        name: 'Dmytro',
+	        surname: 'Shashkov',
+	        gender: 'boy'
+	    },{
+	        id: 4,
+	        name: 'Marian',
+	        surname: 'Kotsylovs\'kyi',
+	        gender: 'boy'
+	    },{
+	        id: 5,
+	        name: 'Oleksandr',
+	        surname: 'Poltorak', 
+	        gender: 'boy'
+	    },{
+	        id: 6,
+	        name: 'Dmytryi',
+	        surname: 'Hun\'ko',
+	        gender: 'boy'
+	    },{
+	        id: 7,
+	        name: 'Oleksandr',
+	        surname: 'Den\'ha',
+	        gender: 'boy'
+	    }],
+
+	    group = new Group(students),
+		tableWrap = helpers.getEl('#students tbody'),
         tableView = new TableView(tableWrap, group.toArray());
 
     return this;
